bc0b4f8bfc41e08485020fd986eaaeb6176b2a12
SVO <svo@qual.is>
2021-06-18 05:34:24 +1000

    Using environment configuration to get access token
    Task: https://trello.com/c/nVzd7H3B

--- golang/server/server.go
+++ golang/server/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/v35/github"
 
+	"github.com/svo/our-source/golang/configuration"
 	"github.com/svo/our-source/golang/model"
 	"github.com/svo/our-source/golang/repository"
 	"github.com/svo/our-source/golang/transformer"
@@ -15,7 +16,7 @@ import (
 
 func Init(factory RouterFactory) *gin.Engine {
 	context := context.Background()
-	sessionContext := model.SessionContext{}.New("coconuts")
+	sessionContext := model.SessionContext{}.New(configuration.EnvironmentConfiguration{}.GetAccessToken())
 	clientFactory := func(httpClient *http.Client) *github.Client { return github.NewClient(httpClient) }
 	gitHubContext := (&repository.GoGitHubContext{}).New(clientFactory, new(transformer.RepositoryTransformer), context, sessionContext)
 	return factory.Build(&gitHubContext)
